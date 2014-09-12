d4660e9104732f6c5009a8d6545f0685ae78876c
Zhuliucao <aether1989@gmail.com>
2014-09-12 17:15:15 +0800

    增加action

---
+++ app/Home/Lib/Action/UserAction.class.php
@@ -0,0 +1,19 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+header("Content-Type:text/html; charset=UTF-8");
+class UserAction extends Action {
+    public function accountModify(){
+
+	    $this->display();
+    }
+    public function myOrder(){
+
+	    $this->display();
+    }    
+    public function myGroup(){
+
+	    $this->display();
+    }   
+    
+} 
+?>
