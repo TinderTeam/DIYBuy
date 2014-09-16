5a3a510fd257d96db01b8fcaa5a2ec1ff56e4a0f
Li Yonglei <liyonglei@fuego.cn>
2014-09-16 08:38:02 +0800

    增加修改密码功能
    Signed-off-by: Li Yonglei <liyonglei@fuego.cn>

--- app/Home/Lib/Action/UserAction.class.php
+++ app/Home/Lib/Action/UserAction.class.php
@@ -3,9 +3,46 @@
 header("Content-Type:text/html; charset=UTF-8");
 class UserAction extends Action {
     public function accountModify(){
-
+		$email=$_SESSION['email'];
+		$this->assign('email',$email);
 	    $this->display();
     }
+	
+	public function modifyPassword(){
+	
+		$email=$_SESSION['email'];
+		$pwd=$_POST['pwd'];
+		$pwd2=$_POST['pwd2'];
+		$user   =   M('user');
+        $condition['email']=$_POST['userName'];
+		if($pwd==$pwd2)
+		{
+			$data['pwd']=$pwd;
+			if($user->create())
+			{
+                if($user->where($condition)->save($data))
+				{
+                    $this->redirect("__APP__/Index/login","",0,"修改成功"); 
+					//$this->success('操作成功！');
+                    //$this->redirect("Admin/administrater","",1,"OK");
+				} 
+				else{
+						$this->assign('email',$email);
+						$this->display('accountModify');
+					}
+				
+            }else{
+                $this->error($user->getError());
+            } 
+		}
+		else
+		{
+			$this->assign('email',$email);
+			$this->display('accountModify');
+		}
+        
+    }
+	
     public function myOrder(){
 
 	    $this->display();
